e74a2e4f7675eedc887ed687f52a3da85dab7801
Julien Atanassian <julien.atanassian@gmail.com>
2022-07-28 15:13:36 -0400

    use route meta fields to refactor router guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Manage from "@/views/Manage.vue";
+import useUserStore from "@/stores/user";
 
 const routes = [
   {
@@ -18,6 +19,9 @@ const routes = [
     name: "manage",
     path: "/manage",
     component: Manage,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/:catchAll(.*)*",
@@ -29,6 +33,21 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   linkExactActiveClass: "text-yellow-500",
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+
+  const store = useUserStore();
+
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: "home" });
+  }
 });
 
 export default router;
