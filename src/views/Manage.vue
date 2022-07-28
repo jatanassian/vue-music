cdbd39e651bf297e4b04766ef5e9b8cd953bc29a
Julien Atanassian <julien.atanassian@gmail.com>
2022-07-28 14:56:37 -0400

    add guard to manage page

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -176,3 +176,20 @@
     </div>
   </section>
 </template>
+
+<script>
+import useUserStore from "@/stores/user";
+
+export default {
+  name: "Manage",
+  beforeRouteEnter(to, from, next) {
+    const store = useUserStore();
+
+    if (store.userLoggedIn) {
+      next();
+    } else {
+      next({ name: "home" });
+    }
+  },
+};
+</script>
