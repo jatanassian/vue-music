2b1530263a9806bfc14e721bcd70af114b955968
Julien Atanassian <julien.atanassian@gmail.com>
2022-07-29 13:46:46 -0400

    add dynamic progress bar

--- src/components/manage/Upload.vue
+++ src/components/manage/Upload.vue
@@ -21,32 +21,14 @@
       </div>
       <hr class="my-6" />
       <!-- Progess Bars -->
-      <div class="mb-4">
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="font-bold text-sm">{{ upload.name }}</div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
           <div
             class="transition-all progress-bar bg-blue-400"
-            style="width: 75%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 35%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 55%"
+            :style="{ width: upload.currentProgress + '%' }"
           ></div>
         </div>
       </div>
@@ -62,6 +44,7 @@ export default {
   data() {
     return {
       isDragOver: false,
+      uploads: [],
     };
   },
   methods: {
@@ -76,7 +59,20 @@ export default {
 
         const storageRef = storage.ref();
         const songsRef = storageRef.child(`songs/${file.name}`);
-        songsRef.put(file);
+        const uploadingFile = songsRef.put(file);
+
+        const uploadsLength = this.uploads.push({
+          uploadingFile,
+          currentProgress: 0,
+          name: file.name,
+        });
+
+        uploadingFile.on("state_changed", (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+          this.uploads[uploadsLength - 1].currentProgress = progress;
+        });
       });
     },
   },
