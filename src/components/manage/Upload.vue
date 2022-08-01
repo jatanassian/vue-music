76477c443a19997343042e5bdcef454fda9f3218
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-01 11:17:09 -0400

    add button to upload file manually

--- src/components/manage/Upload.vue
+++ src/components/manage/Upload.vue
@@ -19,6 +19,7 @@
       >
         <h5>Drop your files here</h5>
       </div>
+      <input type="file" multiple @change="upload($event)" />
       <hr class="my-6" />
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -54,7 +55,10 @@ export default {
     // Upload file to Firebase storage and add sond data to Firestore
     upload(event) {
       this.isDragOver = false;
-      const files = [...event.dataTransfer.files];
+
+      const files = event.dataTransfer
+        ? [...event.dataTransfer.files]
+        : [...event.target.files];
 
       files.forEach((file) => {
         if (file.type !== "audio/mpeg") {
