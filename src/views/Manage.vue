196f42fb51970edbcbb6e18f4abc382972937aad
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-05 18:11:21 -0400

    add directive again

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -8,11 +8,11 @@
         <div
           class="bg-white rounded border border-gray-200 relative flex flex-col"
         >
-          <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+          <div
+            class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+            v-icon="{ icon: 'compact-disc', color: 'green' }"
+          >
             <span class="card-title">My Songs</span>
-            <i
-              class="fa fa-compact-disc float-right text-green-400 text-2xl"
-            ></i>
           </div>
           <div class="p-6">
             <song-list-item
