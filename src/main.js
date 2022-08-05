815591632df4f291760ef7b197509e72cf1a4fc0
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-05 17:36:24 -0400

    remove custom directive

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
-import Icon from "./directives/icon";
 
 import "./assets/base.css";
 import "./assets/main.css";
@@ -19,7 +18,6 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia());
     app.use(router);
     app.use(VeeValidatePlugin);
-    app.directive("icon", Icon);
 
     app.mount("#app");
   }
