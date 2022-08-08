6d1bb8199b12dec93d4c27e6883433b256c65663
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-08 14:22:18 -0400

    add service workers

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
+import { registerSW } from "virtual:pwa-register";
 
 import "./assets/base.css";
 import "./assets/main.css";
+
+registerSW({ immediate: true });
 
 let app;
 
