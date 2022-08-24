0b7f900137a868b47ccebbebc05215a89885b1b6
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-24 12:18:42 -0400

    add test songitems are rendered in order

--- src/components/__tests__/home.spec.js
+++ src/components/__tests__/home.spec.js
@@ -2,6 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import Home from "@/views/Home.vue";
 import Icon from "@/directives/icon";
 import SongItem from "@/components/home/SongItem.vue";
+import { expect } from "vitest";
 
 describe("Home.vue", () => {
   test("renders list of songs", () => {
@@ -19,5 +20,9 @@ describe("Home.vue", () => {
     const items = component.findAllComponents(SongItem);
 
     expect(items).toHaveLength(songs.length);
+
+    items.forEach((wrapper, i) => {
+      expect(wrapper.props().song).toStrictEqual(songs[i]);
+    });
   });
 });
