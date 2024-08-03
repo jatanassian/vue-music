cf912444caf954a71259b77b339c3e2e91b39d6b
Julien Atanassian <julien.atanassian@gmail.com>
2024-08-03 01:19:16 -0400

    clear everything

--- src/includes/helpers.js
+++
@@ -1,8 +0,0 @@
-export default {
-  formatTime(timeInSeconds) {
-    const minutes = Math.floor(timeInSeconds / 60) || 0;
-    const seconds = Math.floor(timeInSeconds - minutes * 60) || 0;
-
-    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  },
-};
--- src/views/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1>About</h1>
-    <p>This project is part of my portfolio.</p>
-    <p>You can see the code on GitHub.</p>
-    <p>Enjoy!</p>
-  </div>
-</template>
