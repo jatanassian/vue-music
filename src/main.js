dbcd56f8a25eb3ffe8457afaffbdda822e1a446b
Julien Atanassian <julien.atanassian@gmail.com>
2024-08-07 17:26:53 -0400

    install and setup firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import VeeValidatePlugin from '@/utils/validation';
+import '@/utils/firebase';
 
 import App from './App.vue';
 import router from './router';
