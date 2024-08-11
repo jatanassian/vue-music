e1b034544f9281dfe2fe8068726986fb5d45d52a
Julien Atanassian <julien.atanassian@gmail.com>
2024-08-11 17:33:55 -0400

    use route names in router links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,21 +2,26 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <RouterLink class="text-white font-bold uppercase text-2xl mr-4" to="/">Music</RouterLink>
+      <RouterLink
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</RouterLink
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
-          <li v-if="!isLoggedIn">
-            <RouterLink class="px-2 text-white" to="/about">About</RouterLink>
+          <li>
+            <RouterLink class="px-2 text-white" :to="{ name: 'about' }">About</RouterLink>
           </li>
           <li v-if="!isLoggedIn">
             <a class="px-2 text-white" href="#" @click="toggleAuthModal">Login / Register</a>
           </li>
           <template v-else>
             <li>
-              <RouterLink class="px-2 text-white" to="/manage">Manage</RouterLink>
+              <RouterLink class="px-2 text-white" :to="{ name: 'manage' }">Manage</RouterLink>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click="logout">Logout</a>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Manage from '@/views/Manage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkExactActiveClass: 'text-yellow-500',
   routes: [
     {
       path: '/',
