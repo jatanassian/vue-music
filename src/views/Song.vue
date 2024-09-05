2b6a00925d231b17b9ae576e45fc214bf71e81de
Julien Atanassian <julien.atanassian@gmail.com>
2024-09-04 20:49:24 -0400

    fix dropdown not set on first load

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -55,6 +55,7 @@
         </VeeForm>
         <!-- Sort Comments -->
         <select
+          :value="$route.query.sort === 'oldest' ? 'oldest' : 'latest'"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
           @change="updateSort"
         >
