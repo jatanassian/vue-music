9d4a64a1eae1eeebb03eb4113a80834465f7cb9a
Julien Atanassian <julien.atanassian@gmail.com>
2024-09-25 00:41:23 +0200

    add rollup visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
+// import { visualizer } from 'rollup-plugin-visualizer';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+    // visualizer({ open: true })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
